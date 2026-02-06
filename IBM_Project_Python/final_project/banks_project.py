4c4168fa74a78cbea5ff682e19a1f2c7b4723530
petrshulgat <s45jxxrmg9@privaterelay.appleid.com>
2026-02-06 15:54:57 +0100

    small debug banks_project.py

--- IBM_Project_Python/final_project/banks_project.py
+++ IBM_Project_Python/final_project/banks_project.py
@@ -39,7 +39,7 @@ def transform(df, csv_path):
 
     df['MC_GBP_Billion'] = [np.round(x*exchange_rate['GBP'],2) for x in df['MC_USD_Billion']]
     df['MC_EUR_Billion'] = [np.round(x*exchange_rate['EUR'],2) for x in df['MC_USD_Billion']]
-    df['MC_USD_Billion'] = [np.round(x*exchange_rate['INR'],2) for x in df['MC_USD_Billion']]
+    df['MC_INR_Billion'] = [np.round(x*exchange_rate['INR'],2) for x in df['MC_USD_Billion']]
 
     return df
 

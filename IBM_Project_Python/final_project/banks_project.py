17c97acf6fb4ba01d9daf57c337c8c0c36a44870
petrs <petrshulgat64@gmail.com>
2026-02-06 15:26:52 +0100

    Initial commit

---
+++ IBM_Project_Python/final_project/banks_project.py
@@ -0,0 +1,100 @@
+import pandas as pd
+from datetime import datetime 
+import numpy as np
+from bs4 import BeautifulSoup
+import requests
+import sqlite3
+
+def log_progress(message):
+    timestamp_format = '%Y-%h-%d-%H:%M:%S'
+    now = datetime.now()
+    timestamp = now.strftime(timestamp_format)
+    with open (log_file, 'a') as f:
+        f.write(timestamp + ' : ' + message + '\n')
+
+def extract(url, table_attribs):
+    df = pd.DataFrame(columns=table_attribs)
+    html_page = requests.get(url).text 
+    data = BeautifulSoup(html_page, 'html.parser')
+
+    tables = data.find_all('tbody')
+    rows = tables[0].find_all('tr')
+    for row in rows:
+        col = row.find_all('td')
+        if len(col) != 0:
+            name = col[1].get_text(strip=True)
+            mc_text = col[2].get_text(strip=True)
+            mc_value = float(mc_text)
+
+            data_dict = {'Name': name,
+                         'MC_USD_Billion': mc_value}
+            df1 = pd.DataFrame(data_dict, index=[0])
+            df = pd.concat([df, df1], ignore_index=True)
+
+    return df 
+
+def transform(df, csv_path):
+    rate_df = pd.read_csv(csv_path)
+    exchange_rate = rate_df.set_index('Currency').to_dict()['Rate']
+
+    df['MC_GBP_Billion'] = [np.round(x*exchange_rate['GBP'],2) for x in df['MC_USD_Billion']]
+    df['MC_EUR_Billion'] = [np.round(x*exchange_rate['EUR'],2) for x in df['MC_USD_Billion']]
+    df['MC_USD_Billion'] = [np.round(x*exchange_rate['INR'],2) for x in df['MC_USD_Billion']]
+
+    return df
+
+def load_to_csv(df, output_path):
+    df.to_csv(output_path)
+
+def load_to_db(df, sql_connection, table_name):
+    df.to_sql(table_name, sql_connection, if_exists='replace', index=False)
+
+def run_query(query_statement, sql_connection):
+    print(query_statement)
+    query_output = pd.read_sql(query_statement, sql_connection)
+    print(query_output)
+
+url = 'https://web.archive.org/web/20230908091635/https://en.wikipedia.org/wiki/List_of_largest_banks'
+table_attribs = ['Name', 'MC_USD_Billion']
+log_file = r'D:\study\Projects\Data_Engineering\IBM_Project_Python\final_project\code_log.txt'
+csv_path = r'D:\study\Projects\Data_Engineering\IBM_Project_Python\final_project\exchange_rate.csv'
+output_path = r'D:\study\Projects\Data_Engineering\IBM_Project_Python\final_project\Largest_banks_data.csv'
+table_name = 'Largest_banks'
+
+log_progress('Preliminaries complete. Initiating ETL process')
+
+df = extract(url, table_attribs)
+
+log_progress('Data extraction complete. Initiating Transformation process')
+
+df = transform(df, csv_path)
+
+log_progress('Data transformation complete. Initiating loading process')
+
+load_to_csv(df, output_path)
+
+log_progress('Data saved to CSV file')
+
+sql_connection = sqlite3.connect('Banks.db')
+
+log_progress('SQL Connection initiated.')
+
+load_to_db(df, sql_connection, table_name)
+
+log_progress('Data loaded to Database as table. Running the query')
+
+query_statement = f"SELECT * FROM Largest_banks"
+
+run_query(query_statement, sql_connection)
+
+query_statement = f"SELECT AVG(MC_GBP_Billion) FROM Largest_banks"
+
+run_query(query_statement, sql_connection)
+
+query_statement = f"SELECT Name from Largest_banks LIMIT 5"
+
+run_query(query_statement, sql_connection)
+
+log_progress('Process Complete.')
+
+sql_connection.close()
